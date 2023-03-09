b3f903f6791c233e053d03d091b924e52c1e0d11
alber <aicarq@hotmail.com>
2023-03-09 15:41:43 -0600

    folder reduced

--- app-calculator.js
+++ app-calculator.js
@@ -1,13 +1,33 @@
-//NUMBERS SELECTOR, CAN BE OPTIMIZED
+// BASIC MATH FUNCTIONS 001
+const add = (a, b) => a += b;
+const subtract = (a, b) => a -= b;
+const multiply = (a, b) => a *= b;
+const divide = (a, b) => a /= b;
 
-//
-const para = document.querySelector('p');
-let displayValue = 0;
-let operatorValue = "";
-let accumulator = 0;
-let secondAccumulator = NaN;
+// CREATE FUNCTION OPERATE 002
+function operate(operator, num1, num2) {
+    let a = Number(num1);
+    let b = Number(num2);
 
+    switch (operator) {
+        case "+":
+            return add(a, b);
+            break;
+        case "-":
+            return subtract(a, b);
+            break;
+        case "*":
+            return multiply(a, b);
+            break;
+        case "/":
+            return divide(a, b);
+            break;
+    }
+}
 
+// CREATE A BASIC CALCULATOR IN HTML AND CSS 003
+
+// CREATE FUNCTIONS THAT POPULATE THE DISPLAY 004
 //NUMBERS SELECTORS
 const selectNumber1 = document.querySelector('.NUMBER-1');
 const selectNumber2 = document.querySelector('#number2');
@@ -21,9 +41,7 @@ const selectNumber9 = document.querySelector('#number9');
 const selectNumber0 = document.querySelector('#number0');
 const floater = document.getElementById('point-floater');
 const numbersGroup = document.getElementById('numbers');
-
 //OPERATORS SELECTORS
-
 const clearAll = document.querySelector('.clear-all');
 const oneDelete = document.querySelector('.delete-one');
 const plusOperator = document.querySelector('#plus');
@@ -32,193 +50,338 @@ const multiplyOperator = document.querySelector('#multiply');
 const dividerOperator = document.querySelector('#divide');
 const equalizer = document.querySelector('#equals');
 
+const para = document.querySelector("p");
+let displayValue = 0;
+let operatorValue = "";
+let accumulator = 0;
+let secondAccumulator = NaN; 
 
 
-// ADD EVENT LISTENERS FOR NUMBERS 
-
-selectNumber0.addEventListener('click', () => {
-    para.textContent += 0;
-    console.log(0);
-    displayValue = para.innerText;
-    consoleLoggersForOperators();
-})
-
-selectNumber1.addEventListener('click', () => {
-    para.textContent += 1;
-    console.log(1);
-    displayValue = para.innerText;
-    consoleLoggersForOperators();
-})
-
-selectNumber2.addEventListener('click', () => {
-    para.textContent += 2;
-    console.log(2);
-    displayValue = para.innerText;
-    consoleLoggersForOperators();
-})
-
-selectNumber3.addEventListener('click', () => {
-    para.textContent += 3;
-    console.log(3);
-    displayValue = para.innerText;
-    consoleLoggersForOperators();
-})
-
-selectNumber4.addEventListener('click', () => {
-    para.textContent += 4;
-    console.log(4);
-    displayValue = para.innerText;
-    consoleLoggersForOperators();
-})
-
-selectNumber5.addEventListener('click', () => {
-    para.textContent += 5;
-    console.log(5);
-    displayValue = para.innerText;
-    consoleLoggersForOperators();
-})
-
-selectNumber6.addEventListener('click', () => {
-    para.textContent += 6;
-    console.log(6);
-    displayValue = para.innerText;
-    consoleLoggersForOperators();
-})
-
-selectNumber7.addEventListener('click', () => {
-    para.textContent += 7;
-    console.log(7);
-    displayValue = para.innerText;
-    consoleLoggersForOperators();
-})
-
-selectNumber8.addEventListener('click', () => {
-    para.textContent += 8;
-    console.log(8);
-    displayValue = para.innerText;
-    consoleLoggersForOperators();
-})
-
-selectNumber9.addEventListener('click', () => {
-    para.textContent += 9;
-    console.log(9);
-    displayValue = para.innerText;
-    consoleLoggersForOperators();
-})
-
-floater.addEventListener('click', () => {
-    para.textContent += '.';
-    console.log('.');
-    displayValue = para.innerText;
-    consoleLoggersForOperators();
-})
-
-// OPERATORS ADD EVENT LISTENERS
-
-clearAll.addEventListener('click', () => {
-    displayValue = Number("");
-    para.textContent = "";
-    accumulator = 0;
-    operatorValue = null;
-    secondAccumulator = 0;
-    equalizer.disabled = false;
-    console.clear();
-});
-
-oneDelete.addEventListener('click', () => {
-    accumulator = displayValue;
-    para.textContent = "";
-    displayValue = 0;
-})
-
-plusOperator.addEventListener('click', () => {
-    console.log('plus operator pressed');
-    operatorValue = '+';
-    accumulator = operate(operatorValue, accumulator, displayValue);
-    para.textContent = accumulator;
-    displayValue = 0;
-})
-
-const operatorValueArray = ["+", "-", "/", "*"];
-
-substractionOperator.addEventListener('click', () => {
-    console.log('subtract operator pressed');
-    operatorValue = '-';
-    toReduceLinerInOperators();
-})
-
-dividerOperator.addEventListener('click', () => {
-    console.log('divider pressed');
-    operatorValue = '/';
-    toReduceLinerInOperators();
-})
-
-multiplyOperator.addEventListener('click', () => {
-    console.log('multiplyer pressed');
-    operatorValue = '*';
-    toReduceLinerInOperators();
-})
-
-equalizer.addEventListener('click', () => {
-    console.log('equals');
-    if (!operatorValue && !accumulator) {
-        accumulator = displayValue;
-        para.textContent = "";
-
+selectNumber0.addEventListener("click", () => {
+    if (Boolean(operatorValue) && !displayValue && para.textContent !== "0.") {
+        console.log(0);
+        para.textContent = 0;
+        displayValue = para.textContent;                
     }
-    para.textContent = operate(operatorValue, accumulator, displayValue);
-    accumulator = operate(operatorValue, accumulator, displayValue);
-    displayValue = para.textContent;
-    consoleLoggersForOperators();
-    equalizer.disabled = true;
+    else {
+        if(!operatorValue && accumulator) {
+            accumulator = 0;
+            console.log(0);
+            para.textContent = 0;
+            displayValue = para.textContent;
+        }
+        else {
+            console.log(0);
+            para.textContent += 0;
+            displayValue = para.textContent;
+        }        
+    }
+})
+selectNumber1.addEventListener("click", () => {
+    if (operatorValue && !displayValue && para.textContent !== "0.") {
+        console.log(1);
+        para.textContent = 1;
+        displayValue = para.textContent;
+                
+    }
+    else {
+        if(!operatorValue && accumulator) {
+            accumulator = 0;
+            console.log(1);
+            para.textContent = 1;
+            displayValue = para.textContent;
+        }
+        else {
+            console.log(1);
+            para.textContent += 1;
+            displayValue = para.textContent;
+        }        
+    }
+})
+selectNumber2.addEventListener("click", () => {
+    if (operatorValue && !displayValue && para.textContent !== "0.") {
+        console.log(2);
+        para.textContent = 2;
+        displayValue = para.textContent;
+                
+    }
+    else {
+        if(!operatorValue && accumulator) {
+            accumulator = 0;
+            console.log(2);
+            para.textContent = 2;
+            displayValue = para.textContent;
+        }
+        else {
+            console.log(2);
+            para.textContent += 2;
+            displayValue = para.textContent;
+        }        
+    }
+})
+selectNumber3.addEventListener("click", () => {
+    if (operatorValue && !displayValue && para.textContent !== "0.") {
+        console.log(3);
+        para.textContent = 3;
+        displayValue = para.textContent;
+                
+    }
+    else {
+        if(!operatorValue && accumulator) {
+            accumulator = 0;
+            console.log(3);
+            para.textContent = 3;
+            displayValue = para.textContent;
+        }
+        else {
+            console.log(3);
+            para.textContent += 3;
+            displayValue = para.textContent;
+        }        
+    }
+})
+selectNumber4.addEventListener("click", () => {
+    if (operatorValue && !displayValue && para.textContent !== "0.") {
+        console.log(4);
+        para.textContent = 4;
+        displayValue = para.textContent;
+                
+    }
+    else {
+        if(!operatorValue && accumulator) {
+            accumulator = 0;
+            console.log(4);
+            para.textContent = 4;
+            displayValue = para.textContent;
+        }
+        else {
+            console.log(4);
+            para.textContent += 4;
+            displayValue = para.textContent;
+        }        
+    }
+})
+selectNumber5.addEventListener("click", () => {
+    if (operatorValue && !displayValue && para.textContent !== "0.") {
+        console.log(5);
+        para.textContent = 5;
+        displayValue = para.textContent;
+                
+    }
+    else {
+        if(!operatorValue && accumulator) {
+            accumulator = 0;
+            console.log(5);
+            para.textContent = 5;
+            displayValue = para.textContent;
+        }
+        else {
+            console.log(5);
+            para.textContent += 5;
+            displayValue = para.textContent;
+        }        
+    }
+})
+selectNumber6.addEventListener("click", () => {
+    if (operatorValue && !displayValue && para.textContent !== "0.") {
+        console.log(6);
+        para.textContent = 6;
+        displayValue = para.textContent;
+                
+    }
+    else {
+        if(!operatorValue && accumulator) {
+            accumulator = 0;
+            console.log(6);
+            para.textContent = 6;
+            displayValue = para.textContent;
+        }
+        else {
+            console.log(6);
+            para.textContent += 6;
+            displayValue = para.textContent;
+        }        
+    }
+})
+selectNumber7.addEventListener("click", () => {
+    if (operatorValue && !displayValue && para.textContent !== "0.") {
+        console.log(7);
+        para.textContent = 7;
+        displayValue = para.textContent;
+                
+    }
+    else {
+        if(!operatorValue && accumulator) {
+            accumulator = 0;
+            console.log(7);
+            para.textContent = 7;
+            displayValue = para.textContent;
+        }
+        else {
+            console.log(7);
+            para.textContent += 7;
+            displayValue = para.textContent;
+        }        
+    }
+})
+selectNumber8.addEventListener("click", () => {
+    if (operatorValue && !displayValue && para.textContent !== "0.") {
+        console.log(8);
+        para.textContent = 8;
+        displayValue = para.textContent;
+                
+    }
+    else {
+        if(!operatorValue && accumulator) {
+            accumulator = 0;
+            console.log(8);
+            para.textContent = 8;
+            displayValue = para.textContent;
+        }
+        else {
+            console.log(8);
+            para.textContent += 8;
+            displayValue = para.textContent;
+        }        
+    }
+})
+selectNumber9.addEventListener("click", () => {
+    if (operatorValue && !displayValue && para.textContent !== "0.") {
+        console.log(9);
+        para.textContent = 9;
+        displayValue = para.textContent;
+                
+    }
+    else {
+        if(!operatorValue && accumulator) {
+            accumulator = 0;
+            console.log(9);
+            para.textContent = 9;
+            displayValue = para.textContent;
+        }
+        else {
+            console.log(9);
+            para.textContent += 9;
+            displayValue = para.textContent;
+        }        
+    }
+})
+floater.addEventListener("click", () => {
+    if (displayValue ) {
+        para.textContent += ".";
+        displayValue = para.textContent;
+    } else {
+        para.textContent = "0.";
+    }
+    console.log(".");    
 })
 
-function consoleLoggersForOperators () {
-    console.log(`accumulator ${accumulator}`);
-    console.log(`operatorValue ${operatorValue}`);
-    console.log(`displayValue ${displayValue}`);
-}
+// NOT NUMBERS
 
-function toReduceLinerInOperators () {
-    accumulator = displayValue;
+clearAll.addEventListener("click", () => {
+    console.log("all cleared");
     para.textContent = "";
+    operatorValue = "";
+    accumulator = 0;
+    secondAccumulator = 0;
     displayValue = 0;
-    equalizer.disabled = false;
-    consoleLoggersForOperators();
-}
+    console.clear();
+})
 
-
-
-
-
-// BASIC MATH FUNCTIONS
-
-const add = (a, b) => a += b;
-
-const subtract = (a, b) => a -= b;
-
-const multiply = (a, b) => a *= b;
-
-const divide = (a, b) => a /= b;
-
-function operate(operator, num1, num2) {
-    let a = Number(num1);
-    let b = Number(num2);
-
-
-    switch (operator) {
-        
-        case '+':
-            return add(a, b);
-            break;
-        case '-':
-            return subtract(a, b);
-            break;
-        case '*':
-            return multiply(a, b);
-            break;
-        case '/':
-            return divide(a, b);
-            break;
+plusOperator.addEventListener("click", () => {
+    if (!accumulator) {       
+        operatorValue = "+";
+        accumulator = displayValue;
+        displayValue = 0;
+    } else if (!operatorValue) {
+        operatorValue = "+";
+        para.textContent = accumulator;
+    } else if (displayValue === 0) {
+        operatorValue = "+";
+    } else {
+        secondAccumulator = displayValue;
+        accumulator = operate(operatorValue, accumulator, displayValue);
+        para.textContent = accumulator;
+        displayValue = 0;
+        operatorValue = "+";
+    }
+})
+substractionOperator.addEventListener("click", () => {
+    if (!accumulator) {       
+        operatorValue = "-";
+        accumulator = displayValue;
+        displayValue = 0;
+    } else if (!operatorValue) {
+        operatorValue = "-";
+        para.textContent = accumulator;
+    } else if (displayValue === 0) {
+        operatorValue = "-";
+    } else {
+        secondAccumulator = displayValue;
+        accumulator = operate(operatorValue, accumulator, displayValue);
+        para.textContent = accumulator;
+        displayValue = 0;
+        operatorValue = "-";
+    } 
+})
+multiplyOperator.addEventListener("click", () => {
+    if (!accumulator) {       
+        operatorValue = "*";
+        accumulator = displayValue;
+        displayValue = 0;
+    } else if (!operatorValue) {
+        operatorValue = "*";
+        para.textContent = accumulator;
+    } else if (displayValue === 0) {
+        operatorValue = "*";
+    } else {
+        secondAccumulator = displayValue;
+        accumulator = operate(operatorValue, accumulator, displayValue);
+        para.textContent = accumulator;
+        displayValue = 0;
+        operatorValue = "*";
     }    
-}
+})
+dividerOperator.addEventListener("click", () => {
+    if (!accumulator) {       
+        operatorValue = "/";
+        accumulator = displayValue;
+        displayValue = 0;
+    } else if (!operatorValue) {
+        operatorValue = "/";
+        para.textContent = accumulator;
+    } else if (displayValue === 0) {
+        operatorValue = "/";
+    } else {
+        secondAccumulator = displayValue;
+        accumulator = operate(operatorValue, accumulator, displayValue);
+        para.textContent = accumulator;
+        displayValue = 0;
+        operatorValue = "/";
+    }     
+})
+
+equalizer.addEventListener("click", () => {
+    if (!accumulator) {
+        accumulator = displayValue;
+        para.textContent = accumulator;
+        displayValue = 0;
+    } else if (!operatorValue) {
+        para.textContent = accumulator;
+    } else if (!displayValue && secondAccumulator !== NaN) {
+        accumulator = operate(operatorValue, accumulator, secondAccumulator);
+        para.textContent = accumulator;
+    } else {
+        accumulator = operate(operatorValue, accumulator, displayValue); 
+        para.textContent = accumulator;
+        secondAccumulator = displayValue;
+        displayValue = 0;
+        console.log("=");
+        console.log(accumulator);
+    }     
+    })
+
+oneDelete.addEventListener("click", () => {
+    para.textContent = para.textContent.split("").splice(0, para.textContent.length-1).join("");
+    displayValue = para.textContent;
+})
